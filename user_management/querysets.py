d3bdc09a1283df7a4313fc9058dc1b8b821eac7f
mmz1380 <mohammad.mesbah.mz.1380@gmail.com>
2023-11-25 10:45:23 +0330

    test of annotations completed

--- user_management/querysets.py
+++ user_management/querysets.py
@@ -1,9 +1,8 @@
-from django.db.models import QuerySet
-
+from django.db.models import *
 
 class CustomerQuerySet(QuerySet):
     def annotate_with_total_spending(self):
-        return self
+        return self.annotate(total_spending=Sum(F'order__total_price')).values()
 
     def annotate_with_order_count(self):
-        return self
+        return self.annotate(order_count=Count(F'order__total_price')).values()
